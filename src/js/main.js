3842bd688ee8fedd3d516cefa1865b7548777049
JoaoSnd <joaopedrosnd@gmail.com>
2025-02-06 12:48:07 -0300

    trying to debug

--- src/js/main.js
+++ src/js/main.js
@@ -48,14 +48,14 @@
     const handlerReceiveEntities = function handlerReceiveEntities(format, elements) {
         if (MashupPlatform.operator.outputs.entityOutput.connected && format === "keyValues") {
             MashupPlatform.wiring.pushEvent("entityOutput", elements)
-            .then(
-                MashupPlatform.operator.log("Output sucessfully", MashupPlatform.log.INFO)
-            );
+                .then(
+                    MashupPlatform.operator.log("Output sucessfully", MashupPlatform.log.INFO)
+                );
         } else if (MashupPlatform.operator.outputs.entityOutput.connected) {
             MashupPlatform.wiring.pushEvent("entityOutput", elements.map(normalize2KeyValue))
-            .then(
-                MashupPlatform.operator.log("Output keyvalue sucessfully", MashupPlatform.log.INFO)
-            );
+                .then(
+                    MashupPlatform.operator.log("Output keyvalue sucessfully", MashupPlatform.log.INFO)
+                );
         }
         if (MashupPlatform.operator.outputs.normalizedOutput && format === "normalized") {
             MashupPlatform.wiring.pushEvent("normalizedOutput", elements);
